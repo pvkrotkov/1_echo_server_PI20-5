26fae1ce147638f0c0f251f39d581ff396ba2934
Kudinova Irina <90453727+YanginFire@users.noreply.github.com>
2021-09-17 16:59:43 +0300

    Update server.py

--- server.py
+++ server.py
@@ -10,11 +10,10 @@ msg = ''
 
 while True:
 	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
+	msg = data.decode()
 	conn.send(data)
-
-print(msg)
+	if (data.decode() == "exit"):
+		break
+	print(msg)
 
 conn.close()
