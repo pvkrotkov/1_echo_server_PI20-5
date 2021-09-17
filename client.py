eaccd39ed9306d096f0731f373eb5408421359f1
NikitaBorisov <nikitaborisov1594@yandex.ru>
2021-09-17 17:50:28 +0300

    job has done!

--- client.py
+++ client.py
@@ -3,14 +3,14 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
+sock.connect(('localhost', 9090))
+while True:
+    msg = input()
+    sock.send(msg.encode())
+    if msg == 'exit':
+        break
+    
+    data = sock.recv(1024)
+    print(data.decode())   
 sock.close()
 
-print(data.decode())
