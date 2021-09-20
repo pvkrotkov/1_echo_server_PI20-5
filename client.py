a0ba8d50da03f0a352da5bd0722ce32ae756e68c
Юлия Дубовицкая <83708760+JuliaHaqq@users.noreply.github.com>
2021-09-20 12:42:23 +0300

    Update client.py

--- client.py
+++ client.py
@@ -3,14 +3,13 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('localhost', 9090))
+while True:
+    msg = input()
+    sock.send(msg.encode())
+    if msg == 'exit':
+        break
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
+    data = sock.recv(1024)
+    print(data.decode())
 sock.close()
-
-print(data.decode())
