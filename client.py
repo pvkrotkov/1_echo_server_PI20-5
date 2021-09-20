00025a1550614e138f935960d621cb965d0d56bf
obadmaev <80482468+obadmaev@users.noreply.github.com>
2021-09-20 11:59:39 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,25 @@
-import socket
-from time import sleep
-
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
+import socket #создали сокет
+print ('создаем сокет')
+sock=socket.socket()
+print('берем сокет с хостом и портом')
+sock.bind(('localhost',9090))#установили хост и сервер
+print('связываем сокет с хостом и портом')
+print('запускаем режим прослушивания')
+sock.listen()
+print('принимаем подключение клиента к серверу')
+conn,addr=sock.accept()
+print(addr)
+msg=''
+print('считываем данные от клиента порциями по кб')
+while True:
+    print('отправка данных клиенту')
+    data=conn.recv(1024)
+    if not data:
+        break
+    msg += data.decode()
+    conn.send(data) 
+    print(msg)
+    msg=''
+print('отключение клиента')
+print('остановка сервера')
+conn.close()
