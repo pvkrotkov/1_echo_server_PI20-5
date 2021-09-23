1b752be532ca324c053859550822c1f0b941d428
Kunasheva <90391164+Kunasheva@users.noreply.github.com>
2021-09-23 11:13:58 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 import socket
-
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
+print ('запуск')
+sock=socket.socket()
+sock.bind(('localhost',9090))
+print('связываем сокет с хостом и портом')
+print('запускаем прослушивание')
+sock.listen()
+print('подключение клиента к серверу')
+conn,addr = sock.accept()
 print(addr)
-
-msg = ''
-
+msg=''
+print('считываем данные от клиента порциями по кб')
 while True:
-	data = conn.recv(1024)
+	print('отправка данных клиенту')
+	data=conn.recv(1024)
 	if not data:
 		break
 	msg += data.decode()
 	conn.send(data)
-
-print(msg)
-
+	print(msg)
+	msg=''
+print('отключение клиента')
+print('остановка сервера')
 conn.close()
