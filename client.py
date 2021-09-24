ee070627dc91233e6ac1c339fb263af87b105383
Nikita <76069143+wlcoder1@users.noreply.github.com>
2021-09-24 14:21:43 +0300

    Гирш Никита

--- client.py
+++ client.py
@@ -1,16 +1,14 @@
 import socket
-from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('localhost', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
+while True:
+    msg = input('Enter a message')
+    if msg =='exit':
+        print('Break connection with a server')
+        break
+    sock.send(msg.encode())
+    data = sock.recv(1024)
+    print(data.encode())
 sock.close()
-
-print(data.decode())
--- server.py
+++ server.py
@@ -1,20 +1,20 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
+sock.bind(("localhost", 9090))
+sock.listen()
 conn, addr = sock.accept()
 print(addr)
 
-msg = ''
+msg = ""
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
+    data = conn.recv(1024)
+    if not data:
+        break
+    msg += data.decode()
+    conn.send(data)
+    print(msg)
+    msg = ""
 
 conn.close()
