17e94d1761dfb58d64583761029e3821668927a1
Гагарин Ярослав <gagarinyaroslav3@gmail.com>
2021-09-29 19:08:16 +0300

    Задание 11 (UDP)

--- client.py
+++ client.py
@@ -1,16 +1,28 @@
-import socket
-from time import sleep
+import socket  # импорт библиотеки сокетов
+from threading import Thread  # импорт потока для многопоточности
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('127.0.0.1', 9090))
 
-while True:
-    line = input()
-    sock.send(line.encode())
-    try:
-        data = sock.recv(1024)
-    except:
-        break
-    print(data.decode())
-sock.close()
+def listenig():  # функция прослушки сервера
+    while True:
+        try:
+            data = sock.recv(1024)
+        except:
+            continue
+        else:
+            print(data.decode())
+    sock.close()
+
+
+def sending():  # функция отправки сообщений на сервер
+    while True:
+        line = input()
+        sock.sendto(line.encode(), server_addr)
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # создание сокета
+server_addr = (input("Введите ip сервера"), int(input("Введите порт сервера")))
+sock.sendto(b"first_try_connection", server_addr)
+thread1 = Thread(target=listenig)  # один поток слушает
+thread2 = Thread(target=sending)  # а другой -- пишет
+thread1.start()
+thread2.start()
--- server.py
+++ server.py
@@ -1,20 +1,17 @@
-import socket
+import socket  # импорт библиотеки сокетов
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
-
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # создание сокета
+self_ip = "127.0.0.1"  # свой ip
+self_port = 9090  # свой порт
+addrs = []  # массив с адресами клиентов
+sock.bind((self_ip, self_port))
 while True:
-	data = conn.recv(1024)
-	decoded = data.decode()
-	if decoded == 'exit':
-		conn.send('Соединение разорвано'.encode())
-		conn.close()
-		break
-	else:
-		msg += data.decode()
-		conn.send(data)
+	data, addr = sock.recvfrom(1024)  # получаем сообщение
+	if addr not in addrs:  # если столкнулись с адресом впервые, добавляем его в список
+		addrs.append(addr)
+	decoded = data.decode()  # расшифровка сообщения
+	if decoded != "first_try_connection":
+		for i in addrs:  # отсылаем присланное сообщение всем известным клиентам
+			tosend = ("client, with ip= "+str(addr[0])+"port= "+str(addr[1])+"said:  "+decoded).encode()
+			sock.sendto(tosend, i)
+
