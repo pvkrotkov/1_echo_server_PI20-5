b0196d44a7971e258c5e2717a023dcc406607c63
Макаров Михаил <63774400+Ara-Bog@users.noreply.github.com>
2021-10-08 12:45:24 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,12 @@
+#!/usr/bin/env python# -*- coding: utf-8 -*-
 import socket
-from time import sleep
-
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
+sock=socket.socket() 
+sock.connect(('localhost', 9090))
+while True:
+    msg = input("text: ")
+    if msg == "exit":
+        break
+    sock.send(msg.encode())  
+    data=sock.recv(1024) 
+    print(data)
+sock.close() 
