e7b850016dcc7b376d0f2dd04542a6eb96bcf98b
Jek4 <90418243+Jek4@users.noreply.github.com>
2021-10-08 14:22:48 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,24 @@
-import socket
-from time import sleep
+@@ -3,14 +3,13 @@
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+ sock = socket.socket()
+ sock.setblocking(1)
+ sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+ #msg = input()
+ msg = "Hi!"
+ sock.send(msg.encode())
 
-data = sock.recv(1024)
+ data = sock.recv(1024)
 
-sock.close()
+ sock.connect(('localhost', 9090))
+ while True:
+     msg = input()
+     sock.send(msg.encode())
+     if msg == 'exit':
+         break
 
-print(data.decode())
+     data = sock.recv(1024)
+     print(data.decode())
+ sock.close()
+
+ print(data.decode())
