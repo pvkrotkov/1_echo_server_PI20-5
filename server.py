c53db47ee5f8018505fb07d028d8fbffe268ea11
obadmaev <80482468+obadmaev@users.noreply.github.com>
2021-10-08 15:07:39 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,25 +1,15 @@
-import socket #создали сокет
-print ('создаем сокет')
-sock=socket.socket()
-print('берем сокет с хостом и портом')
-sock.bind(('localhost',9090))#установили хост и сервер
-print('связываем сокет с хостом и портом')
-print('запускаем режим прослушивания')
-sock.listen()
-print('принимаем подключение клиента к серверу')
-conn,addr=sock.accept()
-print(addr)
-msg=''
-print('считываем данные от клиента порциями по кб')
+import socket
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind (('',9091))
+client = [] # Массив где храним адреса клиентов
+print ('server online!')
 while True:
-    print('отправка данных клиенту')
-    data=conn.recv(1024)
-    if not data:
-        break
-    msg += data.decode()
-    conn.send(data) 
-    print(msg)
-    msg=''
-print('отключение клиента')
-print('остановка сервера')
-conn.close()
+         data ,address = sock.recvfrom(1024)
+         print (address[0], address[1]) #выводим информацию об ip и хосте
+         if address not in client :
+                 client.append(address)
+         for clients in client :
+                 if clients == address :
+                     continue
+                 sock.sendto(data,clients)
+print('Server is shutting down')
