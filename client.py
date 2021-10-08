c90f959fa975ce52281f4258639269087ac3510c
Макаров Михаил <63774400+Ara-Bog@users.noreply.github.com>
2021-10-08 12:49:11 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,14 @@
 import socket
-from time import sleep
+sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
+sock.sendto("check".encode(), ("127.0.0.1", 9090)) 
+if sock.recv(1024).decode() == "False":
+    name = input("Введите ваш ник: ")
+    sock.sendto(name.encode(), ("127.0.0.1", 9090)) 
+while True:
+    msg = input("text: ")
+    if msg == "exit":
+        break
+    sock.sendto(msg.encode(), ("127.0.0.1", 9090)) 
+    print(sock.recv(1024).decode())
+sock.close() 
