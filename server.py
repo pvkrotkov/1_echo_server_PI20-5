d149d00b844dc9e931bf78e84fd928390147ed03
Макаров Михаил <63774400+Ara-Bog@users.noreply.github.com>
2021-10-08 12:45:01 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,13 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
-
+sock=socket.socket() 
+sock.bind(('', 9090)) 
+sock.listen(0) 
+conn, addr=sock.accept() 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+    data=conn.recv(1024) 
+    if not data: 
+        conn, addr=sock.accept() 
+        continue
+    print(data)
+    conn.send(data.upper()) 
