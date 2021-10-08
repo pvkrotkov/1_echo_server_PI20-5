c125f6c71312685397a8d5e69be40a66e034152a
Макаров Михаил <63774400+Ara-Bog@users.noreply.github.com>
2021-10-08 12:50:14 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python# -*- coding: utf-8 -*-
 import socket
-sock=socket.socket() 
-sock.connect(('localhost', 9090))
+sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+
+sock.sendto("check".encode(), ("127.0.0.1", 9090)) 
+if sock.recv(1024).decode() == "False":
+    name = input("Введите ваш ник: ")
+    sock.sendto(name.encode(), ("127.0.0.1", 9090)) 
 while True:
     msg = input("text: ")
     if msg == "exit":
         break
-    sock.send(msg.encode())  
-    data=sock.recv(1024) 
-    print(data)
+    sock.sendto(msg.encode(), ("127.0.0.1", 9090)) 
+    print(sock.recv(1024).decode())
 sock.close() 
