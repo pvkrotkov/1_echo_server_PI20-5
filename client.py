e37710ddbc1dbc59782ec758b57d233b07895adc
Пупынин Савва <pupyninsavva@gmail.com>
2021-10-14 21:08:46 +0300

    Добавлены пояснения

--- client.py
+++ client.py
@@ -1,21 +1,21 @@
 import socket
 from time import sleep
-
+#создаем объект сокета
 sock = socket.socket()
-sock.setblocking(1)
+sock.setblocking(1) #задаем блокирующий параметр
 print("Соединение с сервером")
-sock.connect(('localhost', 9090))
+sock.connect(('localhost', 9090)) #подсоединяемся к порту 9090 локального хоста
 print("Соединено с сервером")
-while True:
-    msg = input("Сообщение серверу: ")
+while True: #отправлдяем сообщения пока не exit
+    msg = input("Сообщение серверу: ") #получаем сообщение от пользователя
     # msg = "Hi!"
     print("Отправка данных серверу")
-    sock.send(msg.encode())
-    if msg == "exit":
+    sock.send(msg.encode()) #кодируем в байты и отправляем сообщение серверу
+    if msg == "exit": #если оно было exit то прерываем цикл
         break
     print("Прием данных от сервера")
-    data = sock.recv(1024)
-    print(data.decode())
+    data = sock.recv(1024) #получаем сообщение от сервера
+    print(data.decode()) #печатаем полученное
 
 print("Разрыв соединения с сервером")
-sock.close()
+sock.close() #закрываем сокет
--- server.py
+++ server.py
@@ -1,28 +1,31 @@
 import socket
 print("Запуск сервера")
+#создаём объект сокета
 sock = socket.socket()
+#биндим сокет для любых подключений через порт 9090
 sock.bind(('', 9090))
+#начинаем прослушиваение
 sock.listen(0)
 print("Начало прослушивания порта")
-conn, addr = sock.accept()
+conn, addr = sock.accept() #получаем подключение от клиента
 print(f"Подключение клиента {addr}")
 
 
-while True:
+while True: #запускаем бесконечный цикл приема и отправки пока не пустое и не exit
 	print("Прием данных от клиента")
-	data = conn.recv(1024)
-	if not data:
+	data = conn.recv(1024) #принимаем 1024 байта информации от клиента
+	if not data: #если пустая, то останавливаем цикл
 		print(f"Отключение клиента {addr}")
 		break
-	msg = data.decode()
-	print(msg)
+	msg = data.decode() #если не пустая преобразовываем из byte в string
+	print(msg) #выводим на экран
 	print("Отправка данных клиенту")
-	conn.send(data)
-	if msg == "exit":
+	conn.send(data) #отправляем обратно клиенту его же сообщение
+	if msg == "exit": #если оно было exit то прерываем цикл
 		print(f"Клиент попросил отключения {addr}")
 		break
 
 
 print("Остановка сервера")
 
-conn.close()
+conn.close() #закрываем соединение
