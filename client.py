42710b5f8a9e848c982810e95ed08d68b6dba388
Daniil <kravchenko5690@gmail.com>
2021-10-22 15:55:32 +0300

    DONE

--- client.py
+++ client.py
@@ -1,16 +1,63 @@
-import socket
+import socket, getpass
 from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+def connect(ip, port):
+    sock = socket.socket()
+    sock.settimeout(1)
+    print("Connecting to the server...")
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+    try:
+        sock.connect(ip, port)
+    except ConnectionRefusedError as err:
+        print(err)
+        return False
+    except TypeError:
+        return False
+    print("Connection accept")
 
-data = sock.recv(1024)
+    while True:
+        try:
+            data = sock.recv(1024)
+        except socket.timeout:
+            break
+        print("Message received")
+        print(data.decode())
 
-sock.close()
+    while True:
+        msg = input("Typing a message to server >>> ")
+        print("\nSending data...")
+        sock.send(msg.encode())
+        if msg == "exit":
+            break
+        try:
+            data = sock.recv(1024)
+        except socket.timeout:
+            continue
+        print("Message received")
+        print(data.decode())
 
-print(data.decode())
+    sock.close()
+    return True
+
+ip = getpass.getpass(promt = "Write the ip address >>> ")
+if ip == "":
+    ip = "192.168.1.69"
+port = getpass.getpass(prompt = "Write the port >>> ")
+if port == "":
+    port = 12345
+else:
+    try:
+        port = int(port)
+    except:
+        print("Incorrect port")
+
+logical = False
+connCount = 0
+while not logical and connCount<5:
+    logical = connect(ip, port)
+    if not logical:
+        connCount += 1
+    else:
+        connCount=0
+if connCount==5:
+    print('Server close. Was 5 attempt to connect')
--- server.py
+++ server.py
@@ -1,20 +1,65 @@
-import socket
+import random, socket, sys
+
+orig_stdout = sys.stdout
+f = open('server_log.txt', 'w')
+sys.stdout = f
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
+portt = 12345
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    try:
+        sock.bind(('', portt))
+        print("Connect to {}".format(portt))
+        break
+    except OSError as err:
+        print("Port {} not avaliable".format(portt))
+        portt = random.randint(1024, 65355)
 
-print(msg)
+sock.listen(0)
+print("Working...")
 
-conn.close()
+def listening(sock):
+    conn, addr = sock.accept()
+    print("Client {} connect".format(addr))
+    with open("client.txt", "a+") as clients:
+        clients.seek(0,0)
+        for i in clients:
+            if addr[0] in i:
+                conn.send("Hello" + i.replace(addr[0], '')).encode()
+                break
+            else:
+                conn.send('Enter your name!'.encode())
+                username = conn.recv(1024).decode()
+                clients.write('\n' + username + addr[0])
+
+    ret = False
+    msg = ""
+
+    while True:
+        print("Data from client")
+        try:
+            data = conn.recv(1024)
+        except (ConnectionResetError, ConnectionAbortedError) as err:
+            print(err, addr)
+            return
+        msg = data.decode()
+        print(msg)
+        if msg == "exit":
+            ret = True
+            break
+        if not data:
+            break
+        conn.send(data)
+        print("Sending data to client...")
+    conn.close()
+    print("Client {} passed out".format(addr))
+    return ret
+
+ret = False
+while not ret:
+    ret = listening(sock)
+print("Stop server")
+
+sys.stdout = orig_stdout
+f.close()
