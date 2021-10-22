f6627184f1885bb2aa0e1f7ba40202b344bd2dbc
MikheevaAA PI20-5 <70269164+nesto4ka@users.noreply.github.com>
2021-10-22 16:19:27 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,31 @@
 import socket
-from time import sleep
-
+print('Cоздается сокет ')
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
+print(' Соединяется с сервером ')
+sock.connect(('localhost', 9090))
+print('Остановка сервера происходит после сообщения выход')
+while True:
+    msg =input()
+    if msg=="выход":
+        print('разрыв соединения с сервером')
+        break
+    print('посылаем данные серверу')
+    sock.send(msg.encode())
+    print('принимаем данные от сервера')
+    data = sock.recv(1024)
+    print(data.decode())
 sock.close()
-
-print(data.decode())
+import threading
+def read_sok():
+     while True:
+        data = sock.recv(1024)
+        print(data.decode('utf-8'))
+server = '127.0.0.1', 9091
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind(('', 0))
+sock.sendto('Соединение с сервером'.encode('utf-8'), server)
+stream = threading.Thread(target= read_sok) # вызываем функцию read_sok
+stream.start()
+while True:
+    message = input()
+    sock.sendto(message.encode('utf-8'), server) #рассылка другим клиентам
