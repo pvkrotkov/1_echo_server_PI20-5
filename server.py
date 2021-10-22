50377abfa985e3fb2e308a0fb0373042f4fa3b75
MikheevaAA PI20-5 <70269164+nesto4ka@users.noreply.github.com>
2021-10-22 16:19:48 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,36 @@
-import socket
-
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
+import socket 
+print ('Создается сокет')
+sock=socket.socket()
+sock.bind(('localhost',9090))#установили хост и сервер
+print('режим прослушивания')
+sock.listen()
+print('принимаем подключение клиента к серверу')
+conn,addr=sock.accept()
 print(addr)
-
-msg = ''
-
+msg=''
+print('считываем данные от клиента порциями по кб')
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind (('',9091))
+client = [] # Массив где храним адреса клиентов
+print ('сервер активен')
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
+    print('отправка данных клиенту')
+    data=conn.recv(1024)
+    if not data:
+        break
+    msg += data.decode()
+    conn.send(data)
+    print(msg)
+    msg=''
+print('отключение клиента')
+print('остановка сервера')
 conn.close()
+data ,address = sock.recvfrom(1024)
+print (address[0], address[1]) #выводим информацию об ip и хосте
+if address not in client :
+    client.append(address)
+    for clients in client :
+        if clients == address :
+            continue
+            sock.sendto(data,clients)
+print('Сервер завершил работу')
